0a45ffc94878f2bd34cb38710e53dbded9aaf52a
Bill Calkins <bill@calkinsconsulting.com>
2020-04-29 19:47:34 -0500

    Fleshed out the network fetcher.

--- HomeNetworkStatus/Fetchers/NetworkStatusFetcher.swift
+++ HomeNetworkStatus/Fetchers/NetworkStatusFetcher.swift
@@ -21,4 +21,83 @@ protocol NetworkFetchable {
     func networkStatus() -> NetworkStatusPublisher
 }
 
+//MARK:- NetworkFetcher
+class NetworkFetcher {
+    
+    private let session: URLSession
+    
+    init() {
+        self.session = URLSession.shared
+    }
+    
+}
+
+//MARK:- NetworkFetch Extension
+extension NetworkFetcher : NetworkFetchable {
+    
+    func networkStatus() -> NetworkStatusPublisher {
+        return self.networkItems(with: self.makeNetworkStatusComps())
+    }
+    
+    //MARK:- Generic Fetch
+    private func networkItems<T>(with components: URLComponents) -> AnyPublisher<T, NetworkPublisherError> where T: Decodable {
+        
+        guard let url = components.url else {
+            let error = NetworkPublisherError.network(description: "Couldn't create URL for Network API")
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        
+        return session.dataTaskPublisher(for: url)
+            .mapError { error in
+                NetworkPublisherError.network(description: error.localizedDescription)
+        }
+        .flatMap { pair in
+            self.decode(pair.data)
+        }
+        .eraseToAnyPublisher()
+        
+    }
+    
+    private func decode<T: Decodable>(_ data: Data) -> AnyPublisher<T, NetworkPublisherError> {
+        
+        let decoder = JSONDecoder()
+        
+        return Just(data)
+            .decode(type: T.self, decoder: decoder)
+            .mapError { error in
+                .decoding(description: (error as? DecodingError).debugDescription)
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    
+}
+
+//MARK:- URL Construction
+private extension NetworkFetcher {
+    
+    struct NetworkServerAPI {
+        static let schema = "https"
+        static let host = "192.168.86.1"
+        static let path = "/api/v1/status"
+        
+    }
+    
+    func makeNetworkStatusComps() -> URLComponents {
+        
+        var components = URLComponents()
+        
+        components.scheme = NetworkServerAPI.schema
+        components.host = NetworkServerAPI.host
+        components.path = NetworkServerAPI.path
+        
+        return components
+    }
+    
+    
+    
+}
+
+
+
 
